0489dbbddaf09c99a72a032245b2a0cb63429483
h0ngliangli <woodyli1982@gmail.com>
2024-10-06 22:49:51 -0700

    重新命名button-toggle-theme

--- base.js
+++ base.js
@@ -1,5 +1,5 @@
-const toggleBtn = document.querySelector("#toggle-btn")
-toggleBtn.addEventListener("click", function () {
+const btnToggleTheme = document.querySelector("#button-toggle-theme")
+btnToggleTheme.addEventListener("click", function () {
   const body = document.querySelector("body")
   body.classList.toggle("dark-mode")
 })
