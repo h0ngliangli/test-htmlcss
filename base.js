f1d088d9d70653c32f9ece508f0046600c4fd5a7
h0ngliangli <woodyli1982@gmail.com>
2024-10-06 21:51:33 -0700

    added content-area

--- base.js
+++ base.js
@@ -1,5 +1,19 @@
-const toggleBtn = document.querySelector('#toggle-btn');
-toggleBtn.addEventListener('click', function (){
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-mode');
+const toggleBtn = document.querySelector("#toggle-btn")
+toggleBtn.addEventListener("click", function () {
+  const body = document.querySelector("body")
+  body.classList.toggle("dark-mode")
+})
+
+const categoryItems = document.querySelectorAll("#category li")
+const contentArea = document.querySelector("#content-area")
+categoryItems.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    event.preventDefault()
+    const url = item.children[0].href
+    fetch(url)
+      .then((response) => response.text())
+      .then((html) => {
+        contentArea.innerHTML = html
+      })
+  })
 })
