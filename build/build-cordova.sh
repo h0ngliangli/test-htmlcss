18b2b1c3accb68cb20df2f2f0e476ee2e8463216
h0ngliangli <woodyli1982@gmail.com>
2024-10-15 14:53:41 -0700

    增加npm run test命令

--- build/build-cordova.sh
+++ build/build-cordova.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf ../test-cordova/MyApp/www/*
 cp -r * ../test-cordova/MyApp/www/
 cd ../test-cordova/MyApp/
 cordova build android
---
+++ build/run-cordova.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../test-cordova/MyApp/
+cordova run android
